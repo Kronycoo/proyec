cc021bdf6095a18ee828c9f59d0ef67fc9240169
Alberto Gutierrez <88956786+Kronycoo@users.noreply.github.com>
2022-02-28 16:26:12 -0600

    Add files via upload

---
+++ Nuv/app.js
@@ -0,0 +1,20 @@
+verificar();
+
+function verificar() {
+    const work = prompt("INGRESA TU NOMBRE").toUpperCase();
+    if(!work) {
+        verificar();
+    }
+    else {
+        escribir(work); 
+    }
+    
+}
+
+function escribir(name) {
+  const typed = new Typed(".typed", {
+    strings: [name],
+    typeSpeed: 100,
+    startDelay: 350,
+  });
+}
